210fa077f501bf98ed75fa4450a57c4cf8d46f3c
maria-edu23 <dudalayti@icloud.com>
2025-12-17 20:26:29 -0300

    Delete controllers directory

--- controllers/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("boutique", "root", "123456789", {
-    host: "localhost",
-    dialect: "mysql",
-    legging: false
-});
-
-module.exports = sequelize;
